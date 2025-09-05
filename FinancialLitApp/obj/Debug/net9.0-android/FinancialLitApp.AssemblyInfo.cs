a782114562e95bb1d56f3b4e9f8eca76762bb2bd
MaccccD <Dumisanimofolo@gmail.com>
2025-09-06 01:25:45 +0200

    Savings Challenge XAML File Update Part 3
    -fixed the UI design for the challenge.

--- FinancialLitApp/obj/Debug/net9.0-android/FinancialLitApp.AssemblyInfo.cs
+++ FinancialLitApp/obj/Debug/net9.0-android/FinancialLitApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FinancialLitApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+19a83f65fe85ec3dc46e96ceb0ba99afc4209586")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+359cb93e8a88a1ddeac211813a342d6451ddb4d0")]
 [assembly: System.Reflection.AssemblyProductAttribute("FinancialLitApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FinancialLitApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
