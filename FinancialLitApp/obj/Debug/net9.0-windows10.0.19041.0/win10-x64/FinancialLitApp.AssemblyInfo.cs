359cb93e8a88a1ddeac211813a342d6451ddb4d0
MaccccD <Dumisanimofolo@gmail.com>
2025-09-05 23:59:17 +0200

    Savings Challenge XAML File Update

--- FinancialLitApp/obj/Debug/net9.0-windows10.0.19041.0/win10-x64/FinancialLitApp.AssemblyInfo.cs
+++ FinancialLitApp/obj/Debug/net9.0-windows10.0.19041.0/win10-x64/FinancialLitApp.AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FinancialLitApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+50a4d13f37f717bc161b5df95fe8400b4ba7a313")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+19a83f65fe85ec3dc46e96ceb0ba99afc4209586")]
 [assembly: System.Reflection.AssemblyProductAttribute("FinancialLitApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FinancialLitApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
