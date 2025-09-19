3d9e01c47fd6922d7d7aba31fc585a8497fe26c9
MaccccD <Dumisanimofolo@gmail.com>
2025-09-19 17:17:24 +0200

    Extra  XAML Pages Updates

--- FinancialLitApp/obj/Debug/net9.0-maccatalyst/maccatalyst-x64/FinancialLitApp.AssemblyInfo.cs
+++ FinancialLitApp/obj/Debug/net9.0-maccatalyst/maccatalyst-x64/FinancialLitApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FinancialLitApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+4fc61b885faa86cf3de68499f2b83cf50d98a795")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+2ce236740b1beac1878500deb6a5ae02bbe25888")]
 [assembly: System.Reflection.AssemblyProductAttribute("FinancialLitApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FinancialLitApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
