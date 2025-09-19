c77985e3e43724059f953479ed5cde4a6b01b3f6
MaccccD <Dumisanimofolo@gmail.com>
2025-09-19 17:38:17 +0200

    NavBar Icons Update

--- FinancialLitApp/obj/Debug/net9.0-ios/iossimulator-x64/FinancialLitApp.AssemblyInfo.cs
+++ FinancialLitApp/obj/Debug/net9.0-ios/iossimulator-x64/FinancialLitApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FinancialLitApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+2ce236740b1beac1878500deb6a5ae02bbe25888")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+3d9e01c47fd6922d7d7aba31fc585a8497fe26c9")]
 [assembly: System.Reflection.AssemblyProductAttribute("FinancialLitApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FinancialLitApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
