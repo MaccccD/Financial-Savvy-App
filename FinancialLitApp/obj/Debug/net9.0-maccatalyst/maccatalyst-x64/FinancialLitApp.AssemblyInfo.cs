86f99b2684ae782c8abeb242a1582a00b7c78566
MaccccD <Dumisanimofolo@gmail.com>
2025-09-29 22:06:01 +0200

    Savings Lesson Testing Updates
    -testing updates

--- FinancialLitApp/obj/Debug/net9.0-maccatalyst/maccatalyst-x64/FinancialLitApp.AssemblyInfo.cs
+++ FinancialLitApp/obj/Debug/net9.0-maccatalyst/maccatalyst-x64/FinancialLitApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FinancialLitApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+ad7594dac4d17353c8717eded1c6c70e8409581c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+4d32c54d570f46ae683fe7c9251f079bce23c800")]
 [assembly: System.Reflection.AssemblyProductAttribute("FinancialLitApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FinancialLitApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
