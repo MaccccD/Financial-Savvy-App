01f394391891d9f48c92a69274e176a75511d0a6
MaccccD <Dumisanimofolo@gmail.com>
2025-09-29 23:32:11 +0200

    Minor Edits Updates
    -testing changes

--- FinancialLitApp/obj/Debug/net9.0-windows10.0.19041.0/win10-x64/FinancialLitApp.AssemblyInfo.cs
+++ FinancialLitApp/obj/Debug/net9.0-windows10.0.19041.0/win10-x64/FinancialLitApp.AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FinancialLitApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+86f99b2684ae782c8abeb242a1582a00b7c78566")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+e6b4cef0b3a3f0351832c615be37b39e705ea453")]
 [assembly: System.Reflection.AssemblyProductAttribute("FinancialLitApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FinancialLitApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
