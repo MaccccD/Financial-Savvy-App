4bc74561909b40653fd36d3943b08cac71f215de
MaccccD <Dumisanimofolo@gmail.com>
2025-10-09 18:15:36 +0200

    Budgeting Challenge Lesson XAML File Update Part 2
    -fixed some missed  typos.
    -resuming challenge and lesson logic part 2

--- FinancialLitApp/obj/Debug/net9.0-windows10.0.19041.0/win10-x64/FinancialLitApp.AssemblyInfo.cs
+++ FinancialLitApp/obj/Debug/net9.0-windows10.0.19041.0/win10-x64/FinancialLitApp.AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FinancialLitApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+7756555119c9f5e4bcd03cd5f401658f73975b30")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+af7ecc4bb0f8b2075c5e1af1a39831935492d318")]
 [assembly: System.Reflection.AssemblyProductAttribute("FinancialLitApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FinancialLitApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
