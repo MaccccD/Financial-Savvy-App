102f42bf6b10727c86e0c41a06980d7bcf43de4c
MaccccD <Dumisanimofolo@gmail.com>
2025-10-14 13:57:55 +0200

    Responsiveness Flex Layout Tag Update

--- FinancialLitApp/obj/Debug/net9.0-android/FinancialLitApp.AssemblyInfo.cs
+++ FinancialLitApp/obj/Debug/net9.0-android/FinancialLitApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FinancialLitApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+6d82732f6c73de692e89012c6e2eb8a616141a88")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+b5708be5905c163cecfbe86e6fff36429b4d2d94")]
 [assembly: System.Reflection.AssemblyProductAttribute("FinancialLitApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FinancialLitApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
