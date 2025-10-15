347ace48fc9e78d66addc07a1e6d78d32469d2b4
MaccccD <Dumisanimofolo@gmail.com>
2025-10-15 20:55:37 +0200

    Budgeting Challenge XAML Page Logic Part 4
    -completed adding the xaml file logic for the challenge

--- FinancialLitApp/obj/Debug/net9.0-android/FinancialLitApp.AssemblyInfo.cs
+++ FinancialLitApp/obj/Debug/net9.0-android/FinancialLitApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FinancialLitApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+d6434c5bc2b1cafaaa444c608ddf5c2ba7f95e98")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+b4b059b1f0d13723df3177804bcc2cdce0e3fcf4")]
 [assembly: System.Reflection.AssemblyProductAttribute("FinancialLitApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FinancialLitApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
