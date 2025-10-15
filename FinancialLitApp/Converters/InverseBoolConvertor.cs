60e5289e7ba9e6b7d3bf1de63fed89163c6eb551
MaccccD <Dumisanimofolo@gmail.com>
2025-10-15 14:27:54 +0200

    Budgeting Challenge Convertors Update Part 2
    -added the rest of the converters logic

--- FinancialLitApp/Converters/InverseBoolConvertor.cs
+++ FinancialLitApp/Converters/InverseBoolConvertor.cs
@@ -4,11 +4,25 @@ using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Globalization;
 
 namespace FinancialLitApp.Converters
 {
-    public class InverseBoolConvertor
+    // this converter is to hide the selected expense items  after they are selected by the player
+    public class InverseBoolConvertor : IValueConverter
     {
-        
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if(value is bool boolValue)
+            {
+                return !boolValue;  // so if the item is selected(true), hide it by returning a hidden response(false) of it in the logic which is like this right here! 
+            }
+            return true; // show the item by default until it is selected.
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- FinancialLitApp/Converters/StringIsNotEmptyConverter.cs
+++ FinancialLitApp/Converters/StringIsNotEmptyConverter.cs
@@ -3,10 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Globalization;
 
 namespace FinancialLitApp.Converters
 {
-    internal class StringIsNotEmptyConverter
+    public class StringIsNotEmptyConverter : IValueConverter
     {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return !string.IsNullOrWhiteSpace(value?.ToString()); //in here im returning a string version of the feedback message by checking if it is not empty
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
