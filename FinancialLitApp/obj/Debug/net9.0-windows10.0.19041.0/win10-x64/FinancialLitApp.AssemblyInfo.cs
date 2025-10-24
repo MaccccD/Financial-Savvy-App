93d644d5efdcd19967b87aa81507232cd28b5987
MaccccD <Dumisanimofolo@gmail.com>
2025-10-24 15:02:53 +0200

    Tax Return Lesson XAML Fille Update Part 3

--- FinancialLitApp/obj/Debug/net9.0-windows10.0.19041.0/win10-x64/FinancialLitApp.AssemblyInfo.cs
+++ FinancialLitApp/obj/Debug/net9.0-windows10.0.19041.0/win10-x64/FinancialLitApp.AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FinancialLitApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+d58ec533043609cdfe7041bb26a4071ad2025b1b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+43d8d20b48875762f369e0e1408f8a6249631bd5")]
 [assembly: System.Reflection.AssemblyProductAttribute("FinancialLitApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FinancialLitApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
