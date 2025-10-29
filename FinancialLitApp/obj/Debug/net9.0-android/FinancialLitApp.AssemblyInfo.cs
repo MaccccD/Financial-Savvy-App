60a6dc2445211b6a47fcce98d927626f1c9fd8c4
MaccccD <Dumisanimofolo@gmail.com>
2025-10-29 13:48:24 +0200

    Tax Return Lesson XAML File Update
    -worked on the design component a bit based on Andre's feedback.
    -adding the points for the tax returns lesson material.

--- FinancialLitApp/obj/Debug/net9.0-android/FinancialLitApp.AssemblyInfo.cs
+++ FinancialLitApp/obj/Debug/net9.0-android/FinancialLitApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FinancialLitApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+6d0e1c25d695043bcf3eb0820efa5ea469539485")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+e1cbab7f67d46921c3e1268f61e278a5b24207d5")]
 [assembly: System.Reflection.AssemblyProductAttribute("FinancialLitApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FinancialLitApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
