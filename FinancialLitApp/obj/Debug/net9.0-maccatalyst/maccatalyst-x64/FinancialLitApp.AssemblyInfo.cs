372c0ce67d5a51f118074694c73342c751a46de1
MaccccD <Dumisanimofolo@gmail.com>
2025-10-31 23:59:51 +0200

    Tax Return XAML Update PART 2

--- FinancialLitApp/obj/Debug/net9.0-maccatalyst/maccatalyst-x64/FinancialLitApp.AssemblyInfo.cs
+++ FinancialLitApp/obj/Debug/net9.0-maccatalyst/maccatalyst-x64/FinancialLitApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FinancialLitApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+1ce653fe5edf7339c961e58e33f71fd832766791")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+b56b1172f65848bb5ebaaf291a2576feefc0bcf4")]
 [assembly: System.Reflection.AssemblyProductAttribute("FinancialLitApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FinancialLitApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
