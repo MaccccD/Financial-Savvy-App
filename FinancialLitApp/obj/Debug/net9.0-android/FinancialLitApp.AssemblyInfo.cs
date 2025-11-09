31ce1e4b1cb702dd81454138cba32a675c16059d
MaccccD <Dumisanimofolo@gmail.com>
2025-11-09 03:29:45 +0200

    Biometric Test Updates

--- FinancialLitApp/obj/Debug/net9.0-android/FinancialLitApp.AssemblyInfo.cs
+++ FinancialLitApp/obj/Debug/net9.0-android/FinancialLitApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FinancialLitApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+da8cef777c42f8d25324e5c3ed37763eab5a9a77")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+2d59eca831baacb442148eeb72a92765d4f7c69c")]
 [assembly: System.Reflection.AssemblyProductAttribute("FinancialLitApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FinancialLitApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
