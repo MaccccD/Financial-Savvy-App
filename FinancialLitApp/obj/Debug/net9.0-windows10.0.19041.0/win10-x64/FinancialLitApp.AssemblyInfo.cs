855a729e0c10282d8ffdc29986d3c89383dd69ab
MaccccD <Dumisanimofolo@gmail.com>
2025-11-09 17:07:58 +0200

    Blockchain Library Updates

--- FinancialLitApp/obj/Debug/net9.0-windows10.0.19041.0/win10-x64/FinancialLitApp.AssemblyInfo.cs
+++ FinancialLitApp/obj/Debug/net9.0-windows10.0.19041.0/win10-x64/FinancialLitApp.AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FinancialLitApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+2d59eca831baacb442148eeb72a92765d4f7c69c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+31ce1e4b1cb702dd81454138cba32a675c16059d")]
 [assembly: System.Reflection.AssemblyProductAttribute("FinancialLitApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FinancialLitApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
