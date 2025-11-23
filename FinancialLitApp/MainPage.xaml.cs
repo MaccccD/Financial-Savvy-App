b4a21792033975eb2d0497cafa2ac524b3883d18
MaccccD <Dumisanimofolo@gmail.com>
2025-11-23 19:49:15 +0200

    Firebase Console Deploy Update Part 2

--- FinancialLitApp/MainPage.xaml.cs
+++ FinancialLitApp/MainPage.xaml.cs
@@ -1,23 +1,23 @@
 ﻿namespace FinancialLitApp;
 
-//public partial class MainPage : ContentPage
-//{
-//	int count = 0;
+public partial class MainPage : ContentPage
+{
+	//int count = 0;
 
-//	public MainPage()
-//	{
-//		InitializeComponent();
-//	}
+	public MainPage()
+	{
+		InitializeComponent();
+	}
 
-//	private void OnCounterClicked(object? sender, EventArgs e)
-//	{
-//		count += 15;
+	//private void OnCounterClicked(object? sender, EventArgs e)
+	//{
+	//	count += 15;
 
-//		if (count == 1)
-//			CounterBtn.Text = $"Clicked {count} time"; // string interpolation
-//		else
-//			CounterBtn.Text = $"Clicked {count} times";
+	//	if (count == 1)
+	//		CounterBtn.Text = $"Clicked {count} time"; // string interpolation
+	//	else
+	//		CounterBtn.Text = $"Clicked {count} times";
 
-//		SemanticScreenReader.Announce(CounterBtn.Text);
-//	}
-//}
+	//	SemanticScreenReader.Announce(CounterBtn.Text);
+	//}
+}
